b92992a26c0b7538775e9baf558e558d93732875
jackmak <jack@reslv.io>
2022-02-03 16:24:22 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/resolve-jack/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  bucket_prefix = "${var.prefix}"
+  bucket = "jackmak521-s3-bucket"
   acl    = "private"
 
   versioning = {
